df1bdc9f8d1350c09970abb4c9853027673d592f
Mario <mariodedo08@gmail.com>
2026-06-19 22:38:06 +0200

    chore: remove debug dependencies and reformat test

--- examples/test.rs
+++ examples/test.rs
@@ -2,13 +2,12 @@ use media_session_reader;
 
 fn main() {
     match media_session_reader::current_track() {
-        Some(track) => match serde_json::to_string_pretty(&track) {
-            Ok(json_string) => println!("{}", json_string),
-            Err(e) => eprintln!("Errore nella serializzazione JSON: {}", e),
-        },
+        Some(track) => {
+            println!("{:#?}", track);
+        }
 
         None => {
-            println!("No player trovato");
+            println!("No player found");
         }
     }
 }
